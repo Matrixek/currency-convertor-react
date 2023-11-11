36de3dc1870921067584dc4c1101a51cf317c97b
Mateusz Pietroń <mpietron357@gmail.com>
2023-11-11 22:11:21 +0100

    delete App.css and change app.js add currnency calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import formlabelText from "./FormlabelText";
+import Container from "./Container";
+import FormHeader from "./FormHeader";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Container>
+            <div className="form_header">
+                <FormHeader/>
+                </div>
+                <p>
+                    <label>
+                        <div className="form__labelText">Podaj kwotę:* (PLN)
+                        <formlabelText/>
+                        </div>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">Wybierz walutę:</span>
+                        <select className=" form__field form__field--select" name="currency">
+                            <option value="EUR">EUR - Euro</option>
+                            <option value="USD">USD - Dolar</option>
+                            <option value="GPB">GPB - Funt brytyjski</option>
+                        </select>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">Wynik:</span>
+                        <input className="form__field" name="value" readonly/>
+                    </label>
+                </p>
+                <p>
+                    * - pole wymagane
+                </p>
+                <p>
+                    <button className="form__button">Przelicz</button>
+                </p>
+                <p>
+                    <button type="reset" className="form__button" name="value">Wyczyść</button>
+                </p>
+                <p>
+                    Kurs z dnia: 21.01.23
+                </p>
+        </Container>
+  ) 
 }
 
 export default App;
