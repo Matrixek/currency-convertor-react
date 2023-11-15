2621252691a314743015ce7d63623ab424f3c2ea
Mateusz Pietroń <mpietron357@gmail.com>
2023-11-15 19:03:00 +0100

    delete folders

--- src/FormHeader/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.css";
-
-const FormHeader = () =>(
-            <fieldset className="header__fieldset">
-                <legend className="header__legend">Kalkulator walut</legend>
-                </fieldset>
-);
-
-export default FormHeader;
