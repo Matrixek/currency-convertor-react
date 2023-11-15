54201e72940aab989f97f8bfa4e95d42334640e7
Mateusz Pietroń <mpietron357@gmail.com>
2023-11-15 19:03:17 +0100

    delete js folder

--- src/FormlabelText/index.js
+++
@@ -1,8 +0,0 @@
-import "./style.css"
-
-const formlabelText = () => (
-    <input type="number" className="form__field" min="0" step="0.01" name="value"
-        placeholder="Wpisz kwotę w zł" required />
-);
-
-export default formlabelText;
--- src/Valut/index.js
+++
@@ -1,7 +0,0 @@
-
-const Valut = [
-    { id: 1, content: "EUR-Euro" },
-    { id: 1, content: "USD-Dolar" },
-    { id: 1, content: "GPB-Funt brytyjski" },
-
-];
