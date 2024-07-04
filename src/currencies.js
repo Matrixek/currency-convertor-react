b482c9d982f9f27167623569447b39e20a009a02
Mateusz Pietroń <mpietron357@gmail.com>
2024-07-04 23:06:41 +0200

    add currencies file

---
+++ src/currencies.js
@@ -0,0 +1,11 @@
+const currencies = [
+    { id: 1, content: "Euro", shrt: "EUR", worth: 4.32 },
+    { id: 2, content: "Dolar Amerykański", shrt: "USD", worth: 4.00 },
+    { id: 3, content: "Funt Brytyjski", shrt: "GBP", worth: 5.15 },
+    { id: 4, content: "Frank Szwajcarski", shrt: "CHF", worth: 4.47 },
+    { id: 5, content: "Japoński Jen", shrt: "JPY", worth: 0.025 },
+    { id: 6, content: "Korona Szwedzka", shrt: "SEK", worth: 0.39 },
+  ];
+  
+  
+  export default currencies;
