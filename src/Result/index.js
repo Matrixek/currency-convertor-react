b30ed76d6f2d6296ff6e829f07a31396fff4aaf0
Mateusz Pietroń <mpietron357@gmail.com>
2024-10-19 23:01:23 +0200

    Result

--- src/Result/index.js
+++ src/Result/index.js
@@ -1,6 +1,6 @@
-const Result = ({ targetAmonut, myResult, selectedCurrency }) => {
-    {result &&
-        {result.setTtargetAmonut}PLN = {result.myResult.toFixed(2)} {result.selectedCurrency}
-    }
+const Result = ({ result }) => {
+    {result && 
+        {result.targetAmonut}PLN = {result.myResult.toFixed(2)} {result.selectedCurrency}
+     }
 };
 export default Result; 
