4a9eac01e5c9ccb77a713b3665aef07e4f845435
Mateusz Pietroń <mpietron357@gmail.com>
2025-01-09 18:48:21 +0100

    change declaration calculateresult

--- src/Form/index.js
+++ src/Form/index.js
@@ -10,90 +10,90 @@ import ResetButton from "../ResetButton/index.js";
 const Form = () => {
     const [amount, setAmount] = useState("");
     const [result, setResult] = useState(null);
-    const [CurrencyValue, setCurrencyValue] = useState({ worth: currencies[0].worth });
+    const [currencyvalue, setCurrencyValue] = useState(currencies[0].worth);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-        calculateResult(amount, CurrencyValue, setCurrencyValue);
+        calculateResult(amount, currencyvalue);
     };
-    const calculateResult = (amount, CurrencyValue, setCurrencyValue) => {
-        const calculationResult = (amount / CurrencyValue) * setCurrencyValue;
+    const calculateResult = (amount, currencyvalue) => {
+        const calculationResult = (amount / currencyvalue);
         setResult(`${calculationResult}`);
-      };
-
-        
-const selectOption = currencies.map((currency) => (
-    <option key={currency.id} value={currency.worth}>
-        {currency.shrt}
-    </option>
-));
+    };
 
 
-return (
-    <form onSubmit={onFormSubmit}>
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">Kalkulator walut</legend>
-            <CurrentDate />
-            <p>
-                <label>
-                    <span className="form__labelText">Podaj kwotę:* (PLN)</span>
-                    <input type="number" className="form__field" min="0" step="0.01" value={amount}
-                        onChange={({ target }) => setAmount(target.value)}
-                        name="value"
-                        placeholder="Wpisz kwotę w zł" required />
+    const selectOption = currencies.map((currency) => (
+        <option key={currency.id} value={currency.worth}>
+            {currency.shrt}
+        </option>
+    ));
 
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span className="form__labelText">Wybierz walutę:</span>
-                    <select className=" form__field form__field--select"
-                        value={CurrencyValue.worth}
-                        onChange={({ target }) => {
-                            const CurrencyValue = currencies.find(currency => currency.worth === parseFloat(target.value));
-                            setCurrencyValue({
-                                worth: CurrencyValue.worth,
-                                shrt: CurrencyValue.shrt,
 
-                            });
-                        }}
-                    >
-                        {selectOption}
-                    </select>
-                </label>
-            </p>
-            <p>
-                <label>
-                    <span className="form__labelText">Wynik:</span>
-                    <p className="form__labelText--result">
-                        {result && (
-                            `${result.targetAmount} PLN = ${(result.myResult.toFixed(2))}
+    return (
+        <form onSubmit={onFormSubmit}>
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Kalkulator walut</legend>
+                <CurrentDate />
+                <p>
+                    <label>
+                        <span className="form__labelText">Podaj kwotę:* (PLN)</span>
+                        <input type="number" className="form__field" min="0" step="0.01" value={amount}
+                            onChange={({ target }) => setAmount(target.value)}
+                            name="value"
+                            placeholder="Wpisz kwotę w zł" required />
+
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">Wybierz walutę:</span>
+                        <select className=" form__field form__field--select"
+                            value={currencyvalue.worth}
+                            onChange={({ target }) => {
+                                const currencyvalue = currencies.find(currency => currency.worth === parseFloat(target.value));
+                                setCurrencyValue({
+                                    worth: currencyvalue.worth,
+                                    shrt: currencyvalue.shrt,
+
+                                });
+                            }}
+                        >
+                            {selectOption}
+                        </select>
+                    </label>
+                </p>
+                <p>
+                    <label>
+                        <span className="form__labelText">Wynik:</span>
+                        <p className="form__labelText--result">
+                            {result && (
+                            `${result.targetAmount} PLN = ${result.calculateResult.toFixed(2)}
                              ${result.selectedCurrency}`
 
-                        )}
-                    </p>
-                </label>
+                            )}
+                        </p>
+                    </label>
 
 
 
-            </p>
-            <p>
-                * - pole wymagane
-            </p>
-            <p>
-                <button className="form__button">Przelicz</button>
+                </p>
+                <p>
+                    * - pole wymagane
+                </p>
+                <p>
+                    <button className="form__button">Przelicz</button>
 
-            </p>
-            <p>
+                </p>
+                <p>
 
-            </p>
-            <ResetButton />
+                </p>
+                <ResetButton />
 
-            <p>
-                Kurs pochodzi ze strony nbp.pl z Tabeli nr 115/A/NBP/2020 z dnia 2020-06-16
-            </p>
-        </fieldset>
-    </form >
-)
+                <p>
+                    Kurs pochodzi ze strony nbp.pl z Tabeli nr 115/A/NBP/2020 z dnia 2020-06-16
+                </p>
+            </fieldset>
+        </form >
+    )
 };
 export default Form;
