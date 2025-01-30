f4b3c1286d54aad9cf4042c0a80df830ee843347
Mateusz Pietroń <mpietron357@gmail.com>
2025-01-30 21:35:18 +0100

    replace component current date from clock index to useCurrentDate

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,36 +1,13 @@
-import { useState, useEffect } from "react";
+import { useCurrentDate } from "./useCurrentDate"
 import "./style.css";
 
-const CurrentDate = ({ date }) => {
-    const [newDate, setNewDate] = useState(new Date());
-   
-    useEffect(() => {
-        const IntervalId = setInterval(() => {
-            setNewDate(new Date());
-        }, 1000);
-        return () => {
-            clearInterval(IntervalId);
-        };
-    }, []);
-   
- date = (newDate.toLocaleString(
-        undefined,
-        {
-            month: "long",
-            weekday: "long",
-            day: "numeric",
-            year: "numeric",
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric"
 
-        },
-    ));
-
+export const Clock = () => {
+    const date = useCurrentDate();
+    
     return (
         <p className="form_date">
             {date}
         </p>
     );
 };
-export default CurrentDate;
