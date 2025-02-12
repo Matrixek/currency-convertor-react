30f227c7057336dfe9e48cd47e99f89b136cad57
Mateusz Pietroń <mpietron357@gmail.com>
2025-02-12 21:58:36 +0100

    add styled.js clock

---
+++ src/Clock/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const FormDate = styled.p`
+    text-align: right;
+    margin: auto;
+    font-size: medium;
+
+@media (max-width:950px) {
+    margin-top: 0px;
+    text-align: center;
+}
+`;
