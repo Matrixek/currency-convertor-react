3789f85c6ebc3e8e3c69c033246fbc63bf45198a
Mateusz Pietroń <mpietron357@gmail.com>
2025-02-13 19:02:59 +0100

    add Formadate to index Clock

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -4,10 +4,21 @@ import { FormDate } from "./styled";
 
 export const Clock = () => {
     const date = useCurrentDate();
-    
+
+    const Formatdate = (date) => date.toLocaleString({
+        undefined,
+        month: "long",
+        weekday: "long",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+
+    });
     return (
         <FormDate>
-            {date}
+            {Formatdate(date)}
         </FormDate>
     );
 };
